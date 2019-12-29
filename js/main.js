e77ac5a4a046208cb5e0552510c0d2669ca37f30
Velibor Markovic <veljko85@users.noreply.github.com>
2019-12-29 20:35:46 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var myImage = document.getElementById("vm");
 var cliWid = document.body.clientWidth;
+var hamBut = document.getElementById("ham-but");
+var firLin = document.getElementById("first-line");
+var secLin = document.getElementById("second-line");
+var thiLin = document.getElementById("third-line");
 
 
 
@@ -7,6 +11,36 @@ var cliWid = document.body.clientWidth;
 //set image on home page size
 myImage.style.height = cliWid / 1.1 + "px";
 myImage.style.width = cliWid / 1.1 + "px";
+
+
+
+function navClose () {
+		firLin.classList.remove("change1");
+		secLin.style.display = "block";
+		thiLin.classList.remove("change2");
+}
+
+function navOpen () {
+	firLin.classList.add("change1");
+	secLin.style.display = "none";
+	thiLin.classList.add("change2");
+}
+
+hamBut.onclick = () => {
+
+		if (secLin.style.display == "none") {
+			navClose();
+
+		}	else	{
+			navOpen();
+		}
+
+}
+
+
+
+
+
 
 
 console.log(cliWid);
