e6e987099b31dcc8e5b46586181318d07ac9bf28
Velibor Markovic <veljko85@users.noreply.github.com>
2019-12-29 19:53:11 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var myImage = document.getElementById("vm");
+var cliWid = document.body.clientWidth;
+
+
+
+
+//set image on home page size
+myImage.style.height = cliWid / 1.1 + "px";
+myImage.style.width = cliWid / 1.1 + "px";
+
+
+console.log(cliWid);
+
+console.log(cliWid / 1.1);
 
